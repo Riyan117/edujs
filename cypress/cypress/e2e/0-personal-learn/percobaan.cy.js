4d74169c4c93b4761042be0ea748f58cc95a26c0
Your Name <your-email@example.com>
2023-07-11 01:03:34 +0700

    added file cypress

---
+++ cypress/cypress/e2e/0-personal-learn/percobaan.cy.js
@@ -0,0 +1,10 @@
+describe('Login to OrangeHRM', () => {
+  it('should successfully login with valid credentials', () => {
+
+    // Custom Commands OrangeHRM
+    cy.OrangeHRM('Admin', 'admin123');
+
+    // Assertions setelah login berhasil
+    cy.get('.oxd-topbar-header-breadcrumb-module').should('contain', 'Dashboard');
+  });
+});
